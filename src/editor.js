436f4f1695a6adaa14e02b571d9db0a6fb6b86dd
Russell Abraham <russell_a@live.com>
2025-04-29 20:30:15 -0700

    Update editor.js

--- src/editor.js
+++ src/editor.js
@@ -1,5 +1,21 @@
+import EditorModel from './editorModel.js';
+import EditorView from './editorView.js';
+import EditorController from './editorController.js';
+
 class Editor {
-    constructor() {
-        // initialize MVC
-    }
+  constructor({ content = '' }) {
+    // Initialize MVC
+    this.model = new EditorModel();
+    this.view = new EditorView(iframe, toolbar, footer, container);
+    this.controller = new EditorController(this.model, this.view);
+    // Set initial content
+    this.controller.setContent(content);
+  }
+  destroy() {
+    this.container.innerHTML = '';
+  }
 }
+
+window.Editor = Editor;
+
+export default Editor;
